eb53b9b6eca3f1bb9ab5d4176395414ce129949a
KoheiYamashita <KoheiYamashita@users.noreply.github.com>
2024-12-28 17:05:03 +0900

    Add docker ci (#2)

--- script/release-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo "Building Docker image..."
-docker build --tag ghcr.io/0235-jp/karkuri-agent-app-dev:latest \
-             --no-cache \
-             --file Dockerfile .
-echo "Pushing Docker image..."
-docker push ghcr.io/0235-jp/karkuri-agent-app-dev:latest
